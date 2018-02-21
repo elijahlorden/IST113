62f9526e6062b7e608501192d8b80b2481692134
elijahlorden <elilorden@gmail.com>
2018-02-21 15:36:32 -0500

    Almost done

--- JsGuess/jscalc.js
+++ JsGuess/jscalc.js
@@ -1,11 +1,32 @@
 
 function start() {
 	var baseline = -1
+	var guess = 0
+	var tries = 0
 	while (typeof(baseline) != "number" || baseline < 0) {
 		baseline = getNumber("Enter a positive number")
 	}
-	alert(baseline)
-	
+	while (guess != baseline) {
+		tries = tries + 1
+		guess = getNumber("Guess the number")
+		//sanity check
+		if (typeof(guess) != "number) { 
+			alert("Invalid guess, input must be a positive integer")
+			continue
+		} else if (Math.floor(Math.abs(guess)) != guess) {
+			alert("Invalid guess, input must be a positive integer")
+			continue
+		}
+		//check if number is higher or lower
+		if (guess > baseline) {
+			alert("Incorrect, your guess was too high.")
+		} else if (guess < baseline) {
+			alert("Incorrect, your guess was too low.")
+		}
+		
+		
+	}
+	alert("Correct, the number was " + baseline + ".\nIt took you " + tries " tries to guess the number.")
 	
 	
 }
