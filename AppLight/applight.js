0f63f9650ee44e531bc571bd4ef3f98c2c074274
elijahlorden <elilorden@gmail.com>
2018-02-28 16:20:04 -0500

    Fix

--- AppLight/applight.js
+++ AppLight/applight.js
@@ -1,14 +1,22 @@
-function lightSwitch(){
-	var x = document.getElementById("toggle").style.cssFloat;
-	
-	if(x == "left"){
-	document.body.style.backgroundColor = "black";
-	document.getElementById("toggle").style.cssFloat = "right";
-	document.getElementById("toggle").innerHTML = "OFF"
+let state = false
+
+let switchElement = document.getElementById("SwitchPart")
+let switchText = document.getElementById("SwitchText")
+let spacerElement = document.getElementById("Spacer1")
+let bodyElement = document.getElementsByTagName("BODY")[0]
+
+
+switchElement.addEventListener("click", function() {
+	state = !state
+	if (state) {
+		switchText.innerHTML = "On"
+		switchElement.style.float = "right"
+		bodyElement.style.background = "#FFFFFF"
+		spacerElement.style.background = "#FFFFFF"
+	} else {
+		switchText.innerHTML = "Off"
+		switchElement.style.float = "left"
+		bodyElement.style.background = "#000000"
+		spacerElement.style.background = "#000000"
 	}
-	else{
-		document.body.style.backgroundColor = "white";
-		document.getElementById("toggle").style.cssFloat = "left";
-		document.getElementById("toggle").innerHTML = "ON"
-	}
-}
+})
